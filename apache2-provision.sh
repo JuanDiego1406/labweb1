b1b76f22ff214e36505761e7631428eadbb75eed
Juan DIego <jmamhua1406@ieszaidinvergeles.org>
2024-11-28 14:41:29 +0100

    second changes

--- apache2-provision.sh
+++ apache2-provision.sh
@@ -7,15 +7,18 @@
 #
 
 # tu código aquí
-
+    cp /vagrant/apolo.olimpo.test.conf /etc/apache2/sites-available/
+    cp /vagrant/atenea.olimpo.test.conf /etc/apache2/sites-available/
 #
 # Habilitar los servidores virtuales
 #
 
 # tu código aquí
-
 #
+    sudo a2dissite 000-default.conf
+    sudo a2ensite apolo.olimpo.test
+    sudo a2ensite atenea.olimpo.test
 # Reiniciar el servicio
 #
-
+    sudo systemctl restart apache2
 # tu código aquí
